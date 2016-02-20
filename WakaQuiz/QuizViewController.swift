1464b7aa1f0975d128bc9f1a93a50dbb9f083468
Niki Mirai <s1411400@coins.tsukuba.ac.jp>
2016-02-20 19:36:24 +0900

    10個でとりあえず作ってみる途中。問題の配列を作った。

--- WakaQuiz/QuizViewController.swift
+++ WakaQuiz/QuizViewController.swift
@@ -11,78 +11,60 @@ import UIKit
 class QuizViewController: UIViewController {
 
     @IBOutlet weak var haikeiImage: UIImageView!
-    
-    var haikeiImg001: UIImage = UIImage(named: "001.jpg")!
-    var haikeiImg002: UIImage = UIImage(named: "002.jpg")!
-    var haikeiImg003: UIImage = UIImage(named: "003.jpg")!
-    var haikeiImg004: UIImage = UIImage(named: "004.jpg")!
-    var haikeiImg005: UIImage = UIImage(named: "005.jpg")!
-    var haikeiImg006: UIImage = UIImage(named: "006.jpg")!
-    var haikeiImg007: UIImage = UIImage(named: "007.jpg")!
-    var haikeiImg008: UIImage = UIImage(named: "008.jpg")!
-    var haikeiImg009: UIImage = UIImage(named: "009.jpg")!
-    var haikeiImg010: UIImage = UIImage(named: "010.jpg")!
-    var haikeiImg011: UIImage = UIImage(named: "011.jpg")!
-    var haikeiImg012: UIImage = UIImage(named: "012.jpg")!
-    var haikeiImg013: UIImage = UIImage(named: "013.jpg")!
-    var haikeiImg014: UIImage = UIImage(named: "014.jpg")!
-    var haikeiImg015: UIImage = UIImage(named: "015.jpg")!
-    var haikeiImg016: UIImage = UIImage(named: "016.jpg")!
-    var haikeiImg017: UIImage = UIImage(named: "017.jpg")!
-    var haikeiImg018: UIImage = UIImage(named: "018.jpg")!
-    var haikeiImg019: UIImage = UIImage(named: "019.jpg")!
-    var haikeiImg020: UIImage = UIImage(named: "020.jpg")!
-    var haikeiImg021: UIImage = UIImage(named: "021.jpg")!
-    var haikeiImg022: UIImage = UIImage(named: "022.jpg")!
-    var haikeiImg023: UIImage = UIImage(named: "023.jpg")!
-    var haikeiImg024: UIImage = UIImage(named: "024.jpg")!
-    var haikeiImg025: UIImage = UIImage(named: "025.jpg")!
-    var haikeiImg026: UIImage = UIImage(named: "026.jpg")!
-    var haikeiImg027: UIImage = UIImage(named: "027.jpg")!
-    var haikeiImg028: UIImage = UIImage(named: "028.jpg")!
-    var haikeiImg029: UIImage = UIImage(named: "029.jpg")!
-    var haikeiImg030: UIImage = UIImage(named: "030.jpg")!
-    var haikeiImg031: UIImage = UIImage(named: "031.jpg")!
-    var haikeiImg032: UIImage = UIImage(named: "032.jpg")!
-    var haikeiImg033: UIImage = UIImage(named: "033.jpg")!
-    var haikeiImg034: UIImage = UIImage(named: "034.jpg")!
-    var haikeiImg035: UIImage = UIImage(named: "035.jpg")!
-    var haikeiImg036: UIImage = UIImage(named: "036.jpg")!
-    var haikeiImg037: UIImage = UIImage(named: "037.jpg")!
-    var haikeiImg038: UIImage = UIImage(named: "038.jpg")!
-    var haikeiImg039: UIImage = UIImage(named: "039.jpg")!
-    var haikeiImg040: UIImage = UIImage(named: "040.jpg")!
-    var haikeiImg041: UIImage = UIImage(named: "041.jpg")!
-    var haikeiImg042: UIImage = UIImage(named: "042.jpg")!
-    var haikeiImg043: UIImage = UIImage(named: "043.jpg")!
-    var haikeiImg044: UIImage = UIImage(named: "044.jpg")!
-    var haikeiImg045: UIImage = UIImage(named: "045.jpg")!
-    var haikeiImg046: UIImage = UIImage(named: "046.jpg")!
-    var haikeiImg047: UIImage = UIImage(named: "047.jpg")!
-    var haikeiImg048: UIImage = UIImage(named: "048.jpg")!
-    var haikeiImg049: UIImage = UIImage(named: "049.jpg")!
-    var haikeiImg050: UIImage = UIImage(named: "050.jpg")!
-    var haikeiImg051: UIImage = UIImage(named: "051.jpg")!
-    var haikeiImg052: UIImage = UIImage(named: "052.jpg")!
-    var haikeiImg053: UIImage = UIImage(named: "053.jpg")!
-    var haikeiImg054: UIImage = UIImage(named: "054.jpg")!
 
-    var haikeiImgArray:[UIImage] = []
+    @IBOutlet weak var questionTextView: UITextView!
     
-    var rnd = Int(arc4random_uniform(54))
+    @IBOutlet weak var answerButton1: UIButton!
+    
+    @IBOutlet weak var answerButton2: UIButton!
+    
+    @IBOutlet weak var answerButton3: UIButton!
+
+    var countAnswer:Int = 0 //とりあえず5問。
+   
+    //背景の画像をランダム表示
+    var haikeiImgArray:[UIImage] = []
+    var rnd: Int = 0
+   
+    //textViewへの問題の表示
+    //！！問題がかぶることが考慮されていない！！
+    var questionArray:[String] = ["秋の田んぼの世話をするための仮の小屋は屋根の編み目が荒く、私の袖が夜露に濡れてしまっていることよ。\n",
+        "天の香具山に白い衣装が干してある。夏が来たようだ。\n",
+        "長い長い夜を愛する人と離れ離れになって一人寂しく寝るのだろうなあ。\n",
+        "田子の浦から遠くを眺めると藤の峰に真っ白な雪が降り積もっているなあ。\n",
+        "人里離れた山の奥の地一面の紅葉を踏みながら鹿の声が聞こえた時は秋がとても寂しく感じられるなあ。\n",
+        "天の川にカササギが掛けていった橋に霜が降りている。その白さを見ると夜が随分更けたなあと思う。\n",
+        "大空を見上げてみると、月がとても美しいことよ。故郷の三笠山にかかっている月でもあるのだなあ。\n",
+        "私が住んでいる家は静かなところにあるので落ち着く。しかし皆さんは私が人付き合いが煩わしいと思ってこの場所に住んでいると思っているみたいですね。\n",
+        "花が色あせるように私の美しさも衰えてしまったわ。恋愛なんかの悩みに、長雨を眺めながら思いを馳せているうちに。\n",
+        "京の都から東国へ行く人も東国から都へ帰る人もこの場所で会って別れていることだ。\n"
+    ]
+    
+    var questionRnd: Int = 0
+    
+    //答えをどのボタンに表示させるか。
+    var selectAnswerRnd: Int = 0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
-        for i in 0..<9 {
-            haikeiImgArray.append(UIImage(named: "00\(i+1).jpg")!)
+        //<--------     背景をランダムに設定　　---------->
+        for i in 0..<54 {
+            haikeiImgArray.append(UIImage(named: String(format: "%03d.jpg",i))!)
         }
-        for i in 9..<54 {
-            haikeiImgArray.append(UIImage(named: "0\(i+1).jpg")!)
-        }
-        
+        rnd = Int(arc4random_uniform(54))
         haikeiImage.image = haikeiImgArray[rnd]
+        
+        //<------------   問題をランダムに設定   ------------->
+        //とりあえず10個の配列でやってみる。あとで変更の必要が有る。!!注意!!
+        //ここってletでいいのか謎です。とりあえずxcodeに注意されたから変えました。
+        questionRnd = Int(arc4random_uniform(10))
+        questionTextView.text = questionArray[Int(questionRnd)]
+        
+        
+        
         
     }
 

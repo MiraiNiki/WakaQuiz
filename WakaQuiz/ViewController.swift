a255c94154f3ea727d87539df0c54bf7fec2557b
Niki Mirai <s1411400@coins.tsukuba.ac.jp>
2016-03-01 00:40:03 +0900

    問題スタートのアニメーション追加

--- WakaQuiz/ViewController.swift
+++ WakaQuiz/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
-import AVFoundation
 
-class ViewController: UIViewController,AVAudioPlayerDelegate {
+class ViewController: UIViewController{
 
+    @IBOutlet weak var questionButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+    }
+    //<-- viewDidLoadは一回だけ呼ばれる。 -->
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let subLabel : UILabel = UILabel(frame: CGRectMake(self.questionLabel.frame.origin.x, self.questionLabel.frame.origin.y, self.questionLabel.frame.width, self.questionLabel.frame.height))
+        subLabel.text = "問題スタート"
+        subLabel.alpha = 1
+        subLabel.textAlignment = NSTextAlignment.Center
+        subLabel.font = UIFont.systemFontOfSize(26)
+        self.view.addSubview(subLabel)
+        
+        UILabel.animateWithDuration(2.0,delay: 0.0, options:  UIViewAnimationOptions.Repeat ,animations: {() -> Void in
+            subLabel.alpha = 0.0
+            }, completion: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,12 +38,13 @@ class ViewController: UIViewController,AVAudioPlayerDelegate {
     }
 
     @IBAction func questionButtonPushed(sender: AnyObject) {
-
+        questionLabel.alpha = 1.0
 
     }
     
     @IBAction func detailButtonPushed(sender: AnyObject) {
 
+        questionLabel.alpha = 1.0
     }
     
 
